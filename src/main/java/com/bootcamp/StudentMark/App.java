7330859be65c778e3eb75f179bdfb4426afb8949
Kumar <skumar15@lm-maa-22004998.paypalcorp.com>
2018-08-06 09:42:52 +0530

    inital commit

---
+++ src/main/java/com/bootcamp/StudentMark/App.java
@@ -0,0 +1,13 @@
+package com.bootcamp.StudentMark;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
